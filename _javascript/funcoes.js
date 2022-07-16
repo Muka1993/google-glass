f69d51e50a723090e740dc29cd2ca616d94ecfd1
Samuel Candido <92275912+Muka1993@users.noreply.github.com>
2022-07-15 22:02:30 -0300

    Restruturando código

--- _javascript/funcoes.js
+++ _javascript/funcoes.js
@@ -3,3 +3,8 @@ function mudaFoto(foto)
     document.getElementById("icone").src = foto;
 }
 
+function myFunction(){
+    var element = document.body;
+    document.body.classList.toggle('dark-mode');
+  }
+  
